205b7adcd1efd82cd898f21a865e78eac34af1e4
Finleyh <finley.d.hammond@outlook.com>
2026-05-12 14:20:41 -0400

    updated trino for kwargs, updated test_trino.py to evaluate it

--- dev_env/trino/test_trino.py
+++ dev_env/trino/test_trino.py
@@ -4,7 +4,6 @@ from typing import Dict, Any
 
 env_config: Dict[str, Any] = combine_env_configs()
 
-
 client = TrinoConnector(
     host=env_config["TRINO_HOST"],
     port=env_config["TRINO_PORT"],
@@ -12,9 +11,17 @@ client = TrinoConnector(
 )
 
 # Find
+print(client.query("SHOW CATALOGS"))
 print(client.query("SELECT * FROM pg.public.employees"))
-
-
 mylist = [{'name':'Pat','department':'Facilities MGMT'}]
-
 print(client.bulk_insert('pg.public.employees',mylist))
+
+
+client_https = TrinoConnector(
+    host=env_config["TRINO_HOST_2"],
+    port=env_config["TRINO_PORT_2"],
+    user=env_config["TRINO_USER_2"],
+    http_scheme='https'
+)
+
+print(client_https.query("SHOW CATALOGS"))
